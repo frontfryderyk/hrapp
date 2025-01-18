36b323eea27eac0ed136e1fa6b66f4d4ca2e3a71
ff <federico.f.salsero@gmail.com>
2025-01-18 09:21:27 +0100

    compose2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,12 @@
-insert into UserType
+INSERT INTO user_type (id, type) VALUES (1, 'ADMIN');
+INSERT INTO user_type (id, type) VALUES (1, 'EMPLOYEE');
+
+
+INSERT INTO users (username, password, salt, user_type_id, rating)
+VALUES ('admin', 'admin123', 'salt',1 , 5.0)
+
+
+INSERT INTO projects (name, description) VALUES
+                                             ('Projekt A', 'Opis projektu A'),
+    ('Projekt B', 'Opis projektu B')
+    ('Projekt C', 'Opis projektu C');
